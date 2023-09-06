2184a47c50d0424ebe8db5be74687a037c19bac7
rodriquelali <rodriquelali@gmail.com>
2023-09-06 10:01:22 -0400

    Poo, suma de dos numeros

---
+++ poo/app/src/main/java/com/example/poo/clases/Calculadora.java
@@ -0,0 +1,20 @@
+package com.example.poo.clases;
+
+public class Calculadora {
+    /*public int a;
+    public int b;
+
+    public Calculadora(int a, int b){
+        this.a = a;
+        this.b =b;
+    }*/
+
+    public int suma(int a, int b){
+        return a + b;
+    }
+
+    public double suma(double a, double b){
+        return a + b;
+    }
+
+}
